6520441a0abb7ea47c62b1b463de1cbf335eec3d
kyle <kyle@panicky.net>
2022-06-23 20:29:33 -0600

    Add date dividers to ActivityList

--- src/ActivityList.jsx
+++ src/ActivityList.jsx
@@ -2,14 +2,38 @@ import React from 'react';
 
 import ActivityFeedItem from './ActivityFeedItem.jsx';
 
-const ActivityList = ({ activities }) => {
-  return (
-    <div>
-      {activities.map((activity) => (
+const DateDivider = ({ date }) => {
+  return <div className="separator">{date}</div>;
+};
+
+const insertDateDividers = (activities) => {
+  const byDate = {};
+
+  activities.forEach((activity) => {
+    const date = new Date(activity.created_at);
+    const dateString = `${date.getFullYear()}-${date.getMonth()}-${date.getDay()}`;
+    byDate[dateString] = (byDate[dateString] || []).concat([activity]);
+  });
+
+  const makeElementArrayForDate = (date) => {
+    const activities = byDate[date];
+
+    return [
+      <DateDivider key={`date-${date}`} date={date} />,
+      ...activities.map((activity) => (
         <ActivityFeedItem key={`activity-${activity.id}`} activity={activity} />
-      ))}
-    </div>
-  );
+      )),
+    ];
+  };
+
+  return Object.keys(byDate)
+    .sort((a, b) => b.localeCompare(a))
+    .map(makeElementArrayForDate)
+    .reduce((acc, x) => acc.concat(x), []);
+};
+
+const ActivityList = ({ activities }) => {
+  return <div>{insertDateDividers(activities)}</div>;
 };
 
 export default ActivityList;
